cb4368218f2be2ee268cc7606571f6cf9d5b43a5
Lavinia Fonte <lavifonte@hotmail.com>
2022-07-11 20:13:53 -0300

    basic text editor with edit and open file options

---
+++ Text/FileOptions.cs
@@ -0,0 +1,55 @@
+﻿namespace TextEditor
+{
+    internal class FileOptions
+    {
+        public static void OpenFile()
+        {
+            Console.Clear();
+            Console.WriteLine("What's the path of the file you want to open?");
+            string path = Console.ReadLine();
+            using (StreamReader sr = new(path))
+            {
+                string text = sr.ReadToEnd(); // reads the entire file in that path
+                Console.WriteLine(text);
+            }
+
+            Console.WriteLine();
+            Console.ReadLine();
+            Screen.Menu();
+        }
+
+        public static void EditFile()
+        {
+            Console.Clear();
+            Console.WriteLine("Type your text below. Press ESC to exit.");
+            Console.WriteLine("---------------------");
+            string text = "";
+
+            do
+            {
+                text += Console.ReadLine();
+                text += Environment.NewLine; // so the system understands each read line as a new line and doesn't squeeze everything together without break lines 
+            }
+            while (Console.ReadKey().Key != ConsoleKey.Escape);
+
+            SaveFile(text);
+        }
+
+        public static void SaveFile(string text)
+        {
+            Console.Clear();
+            Console.WriteLine("What's the destination path for your file?");
+            var path = Console.ReadLine();
+
+            using (StreamWriter sw = new(path)) // using() makes sure the file is closed afterwards
+            {
+                sw.Write(text);
+            }
+
+            Console.WriteLine($"File {path} successfully saved.");
+            Console.ReadLine();
+            Screen.Menu();
+
+        }
+    }
+}
---
+++ Text/Screen.cs
@@ -0,0 +1,26 @@
+﻿namespace TextEditor
+{
+    internal class Screen
+    {
+        public static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("Hey! What would you like to do? Type chosen number. Example: 1");
+            Console.WriteLine("#0: exit");
+            Console.WriteLine("#1: open file");
+            Console.WriteLine("#2: create new file");
+
+            int option = int.Parse(Console.ReadLine());
+
+            switch (option)
+            {
+                case 0: System.Environment.Exit(0); break;
+                case 1: FileOptions.OpenFile(); break;
+                case 2: FileOptions.EditFile(); break;
+                default: Menu(); break;
+            }
+        }
+
+
+    }
+}
